b511febc7d9d342abc37cf3deb4b90552099d40f
Matheus Lima <mlbp@cin.ufpe.br>
2022-06-06 11:20:23 -0300

    Adding simple header

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import { HeaderStyles } from "./HeaderStyles";
+
+const Header = () => {
+    return (
+        <HeaderStyles>
+            Wordle!
+        </HeaderStyles>
+    );
+}
+ 
+export default Header;
