dea381838981fbd7f38b155297fb74b203ef66dd
Matheus Lima <mlbp@cin.ufpe.br>
2022-06-07 10:59:52 -0300

    Adding components for individual cells

---
+++ src/Grid/Row/Cell/Cell.js
@@ -0,0 +1,12 @@
+import { CellStyles } from "./CellStyles";
+
+
+const Cell = () => {
+    return (
+        <CellStyles>
+            
+        </CellStyles>        
+    );
+}
+ 
+export default Cell;
---
+++ src/Grid/Row/Cell/CellStyles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CellStyles = styled.li`
+    border: 4px solid #b5b5b5;
+    background-color: #f5f5f5;
+    height: 60px;
+    width: 60px;
+    list-style-type: none;
+    display: inline;
+    border-radius: 4px;
+    padding: 5px;
+    font-size: 35px;
+    font-weight: bold;
+`;
