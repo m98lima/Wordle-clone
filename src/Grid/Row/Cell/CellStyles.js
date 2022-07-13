73edf834f26b29110cb39cb1dbc12fa9fc834f8e
Matheus Lima <mlbp@cin.ufpe.br>
2022-07-13 11:54:30 -0300

    Fixing display of cells

--- src/Grid/Row/Cell/CellStyles.js
+++ src/Grid/Row/Cell/CellStyles.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 export const CellStyles = styled.li`
     border: 4px solid #b5b5b5;
     background-color: #f5f5f5;
-    height: 60px;
-    width: 60px;
-    display: inline;
+    height: 30px;
+    width: 30px;
+    display: inline-block;
     border-radius: 4px;
     padding: 5px;
+    padding-bottom: 12px;
     margin-inline: 2px;
     font-size: 35px;
     font-weight: bold;
+    text-align: center;
 `;
