b162e38dad37c91a4730fd62ef5df580ca54bd18
Matheus Lima <mlbp@cin.ufpe.br>
2022-07-24 15:56:26 -0300

    Adding grid to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './Header/Header'
+import Grid from './Grid/Grid'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Grid />
     </div>
   );
 }
