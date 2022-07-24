545974c33910f3923007fbdf8f707cdc05a8dbb3
Matheus Lima <mlbp@cin.ufpe.br>
2022-07-24 15:24:49 -0300

    Adding components for the grid

---
+++ src/Grid/Grid.js
@@ -0,0 +1,18 @@
+import {GridStyles} from "./GridStyles"
+import Row from "./Row/Row";
+
+
+const Grid = () => {
+    return (
+        <GridStyles>
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+        </GridStyles>
+    );
+}
+ 
+export default Grid;
---
+++ src/Grid/GridStyles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const GridStyles = styled.section`
+    margin: 45px auto 30px;
+`;
