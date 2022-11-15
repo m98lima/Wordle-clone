c99615a97e6a4a69cfec082c543d69bf3ab8d0f7
Matheus Lima <mlbp@cin.ufpe.br>
2022-11-15 12:55:16 -0300

    Adding functionality to letter key presses

--- src/App.js
+++ src/App.js
@@ -7,13 +7,26 @@ function App() {
   const [prevRows, setPrevRows] = useState([]);
   const [currRow, setCurrRow] = useState(["", "", "", "", ""]);
   const [emptyRows, setEmptyRows] = useState(5);
+  const [currIndex, setCurrIndex] = useState(0);
 
+  let tempCurrRow;
+
+  const handleKeyPress = (key) => {
+    return () => {
+      if (currIndex < 5) {
+        tempCurrRow = currRow;
+        tempCurrRow[currIndex] = key;
+        setCurrIndex(currIndex + 1);
+        setCurrRow(tempCurrRow);
+      };
+    };
+  };
 
   return (
     <div className="App">
       <Header />
       <Grid prevRows={prevRows} currRow={currRow} rowAmount={emptyRows} />
-      <Keyboard />
+      <Keyboard handleKeyPress={handleKeyPress} />
     </div>
   );
 }
--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -2,25 +2,25 @@ import { keys } from "../../util/Keyboard";
 import { KeyboardContainer, KeyContainer, KeyRowContainer, SpecialKeyContainer } from "./KeyboardStyles";
 
 
-const Keyboard = () => {
+const Keyboard = ({ handleKeyPress }) => {
   return (
     <KeyboardContainer>
       <KeyRowContainer>
         {keys[0].map((key) => (
-          <KeyContainer>{key}</KeyContainer>
+          <KeyContainer onClick={handleKeyPress(key)} >{key}</KeyContainer>
         ))}
       </KeyRowContainer>
 
       <KeyRowContainer>
         {keys[1].map((key) => (
-          <KeyContainer>{key}</KeyContainer>
+          <KeyContainer onClick={handleKeyPress(key)} >{key}</KeyContainer>
         ))}
       </KeyRowContainer>
 
       <KeyRowContainer>
         <SpecialKeyContainer>{"Back"}</SpecialKeyContainer>
         {keys[2].map((key) => (
-          <KeyContainer>{key}</KeyContainer>
+          <KeyContainer onClick={handleKeyPress(key)} >{key}</KeyContainer>
         ))}
         <SpecialKeyContainer>{"Enter"}</SpecialKeyContainer>
       </KeyRowContainer>
